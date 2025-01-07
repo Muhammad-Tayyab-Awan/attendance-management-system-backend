34dcdac3271b0ce4988d255a7898f7fb69920f6b
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-01-07 09:44:49 +0500

    added admin role verification for user registration and enhanced cookie handling

--- routes/User.js
+++ routes/User.js
@@ -1,7 +1,7 @@
 import express from "express";
 import User from "../models/Users.js";
 import bcrypt from "bcryptjs";
-import { body, param, validationResult } from "express-validator";
+import { body, cookie, param, validationResult } from "express-validator";
 import jwt from "jsonwebtoken";
 import mailTransporter from "../utils/mailTransporter.js";
 
@@ -20,7 +20,6 @@ router.post(
       .isString()
       .matches(/^[a-z0-9]{6,18}$/),
     body("email", "Enter a valid email").isEmail(),
-    body(),
     body(
       "password",
       "Password must contain at least 8 chars and must consist of min 2 uppercase, lowercase, numbers and symbols"
@@ -37,41 +36,112 @@ router.post(
     body("gender").isString().isIn(["male", "female"]),
     body("address")
       .isString()
-      .matches(/^[a-zA-Z0-9\s,.\-]{10,70}$/)
+      .matches(/^[a-zA-Z0-9\s,.\-]{10,70}$/),
+    cookie("admin-auth-token").isJWT().optional()
   ],
   async (req, res) => {
     try {
       const result = validationResult(req);
       if (result.isEmpty()) {
-        const { password } = req.body;
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password, salt);
-        req.body.password = hashedPassword;
-        User.create(req.body).then(async (user) => {
-          const verificationToken = jwt.sign({ userId: user.id }, JWT_SECRET);
-          const htmlMessage = `<h2>Dear ${user.firstName} ${user.lastName}! Please verify your email by visiting the link below</h2><a href="${API_URI}/api/user/verify-user/${verificationToken}">Verify Now</a>`;
-          mailTransporter.sendMail(
-            {
-              to: user.email,
-              subject: "User Email Verification",
-              html: htmlMessage
-            },
-            (err) => {
-              if (err) {
-                res.status(500).json({
-                  success: false,
-                  error: "Error Occurred on Server Side",
-                  message: err.message
-                });
+        if (req.cookies["admin-auth-token"]) {
+          const token = req.cookies["admin-auth-token"];
+          jwt.verify(token, JWT_SECRET, async (err, decodedToken) => {
+            if (err) {
+              res
+                .status(401)
+                .json({ success: false, error: "Invalid request" });
+            } else {
+              const user = await User.findById(decodedToken.userId);
+              if (user && user.role === "admin") {
+                const { password } = req.body;
+                const salt = await bcrypt.genSalt(10);
+                const hashedPassword = await bcrypt.hash(password, salt);
+                req.body.password = hashedPassword;
+                User.create(req.body)
+                  .then(async (user) => {
+                    const verificationToken = jwt.sign(
+                      { userId: user.id },
+                      JWT_SECRET
+                    );
+                    const htmlMessage = `<h2>Dear ${user.firstName} ${user.lastName}! Please verify your email by visiting the link below</h2><a href="${API_URI}/api/user/verify-user/${verificationToken}">Verify Now</a>`;
+                    mailTransporter.sendMail(
+                      {
+                        to: user.email,
+                        subject: "User Email Verification",
+                        html: htmlMessage
+                      },
+                      (err) => {
+                        if (err) {
+                          res.status(500).json({
+                            success: false,
+                            error: "Error Occurred on Server Side",
+                            message: err.message
+                          });
+                        } else {
+                          res.status(201).json({
+                            success: true,
+                            msg: `User Registration Successful. We have sent verification email to ${user.email}`
+                          });
+                        }
+                      }
+                    );
+                  })
+                  .catch(() => {
+                    res.status(400).json({
+                      success: false,
+                      error: "Error occurred while registering new user account"
+                    });
+                  });
               } else {
-                res.status(201).json({
-                  success: true,
-                  msg: `User Registration Successful. We have sent an email to ${user.email}, please visit your mailbox to verify your account`
+                res.status(401).json({
+                  success: false,
+                  error: "Invalid request"
                 });
               }
             }
-          );
-        });
+          });
+        } else {
+          const { password } = req.body;
+          const salt = await bcrypt.genSalt(10);
+          const hashedPassword = await bcrypt.hash(password, salt);
+          req.body.password = hashedPassword;
+          req.body.role = "user";
+          User.create(req.body)
+            .then(async (user) => {
+              const verificationToken = jwt.sign(
+                { userId: user.id },
+                JWT_SECRET
+              );
+              const htmlMessage = `<h2>Dear ${user.firstName} ${user.lastName}! Please verify your email by visiting the link below</h2><a href="${API_URI}/api/user/verify-user/${verificationToken}">Verify Now</a>`;
+              mailTransporter.sendMail(
+                {
+                  to: user.email,
+                  subject: "User Email Verification",
+                  html: htmlMessage
+                },
+                (err) => {
+                  if (err) {
+                    res.status(500).json({
+                      success: false,
+                      error: "Error Occurred on Server Side",
+                      message: err.message
+                    });
+                  } else {
+                    res.status(201).json({
+                      success: true,
+                      msg: `User Registration Successful. We have sent an email to ${user.email}, please visit your mailbox to verify your account`
+                    });
+                  }
+                }
+              );
+            })
+            .catch(() => {
+              res.status(400).json({
+                success: false,
+                error: "Error occurred while registering your account"
+              });
+            });
+        }
       } else {
         res.status(400).json({
           success: false,
