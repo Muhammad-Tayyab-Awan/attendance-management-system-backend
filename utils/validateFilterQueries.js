cc208a1eb396ca789b2bd1c525ae09c03ebfd12b
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-01-08 23:04:06 +0500

    fix: updated validation logic for filter queries to use allowed query length

--- utils/validateFilterQueries.js
+++ utils/validateFilterQueries.js
@@ -1,6 +1,6 @@
 function validateFilterQueries(queries, allowedQueries) {
   const keys = Object.keys(queries);
-  if (keys.length < 0 || keys.length > 6) {
+  if (keys.length < 0 || keys.length > allowedQueries.length) {
     return false;
   }
   for (const key of keys) {
