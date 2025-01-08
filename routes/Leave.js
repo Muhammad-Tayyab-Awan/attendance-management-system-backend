bf48b0e7b6bdfe33a7123593e00dec2dcbd5e934
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-01-08 23:19:14 +0500

    feat: added admin route for retrieving and filtering leave requests

--- routes/Leave.js
+++ routes/Leave.js
@@ -417,4 +417,139 @@ router.get(
   }
 );
 
+router.get(
+  "/admin",
+  verifyAdminLogin,
+  [
+    query("filter")
+      .isString()
+      .isIn(["today", "past", "upcoming", "week", "month", "year"])
+      .optional(),
+    query("status")
+      .isString()
+      .isIn(["pending", "approved", "rejected"])
+      .optional(),
+    query("reason")
+      .isString()
+      .isIn(["medical", "personal", "academic", "other"])
+      .optional(),
+    query("startDate")
+      .isISO8601({
+        strict: true,
+        strictSeparator: true
+      })
+      .optional(),
+    query("endDate")
+      .isISO8601({
+        strict: true,
+        strictSeparator: true
+      })
+      .optional(),
+    query("leaveId").isMongoId().optional(),
+    query("userId").isMongoId().optional()
+  ],
+  async (req, res) => {
+    try {
+      const result = validationResult(req);
+      if (result.isEmpty()) {
+        const queries = req.query;
+        if (Object.keys(queries).length === 0) {
+          const leaves = await Leave.find()
+            .populate("userId", ["email", "_id"], "user")
+            .select("-__v");
+          return res.status(200).json({ success: true, allLeaves: leaves });
+        } else if (
+          validateFilterQueries(queries, [
+            "filter",
+            "reason",
+            "status",
+            "startDate",
+            "endDate",
+            "leaveId",
+            "userId"
+          ])
+        ) {
+          if (queries.leaveId) {
+            const leave = await Leave.findById(queries.leaveId)
+              .populate("userId", ["email", "_id"], "user")
+              .select("-__v");
+            if (leave) {
+              return res.status(200).json({ success: true, leave });
+            } else {
+              return res.status(400).json({
+                success: false,
+                error: "Invalid leaveId"
+              });
+            }
+          }
+          if (queries.filter) {
+            let today =
+              new Date().toISOString().split("T")[0] + "T00:00:00.000Z";
+            if (queries.filter === "today") {
+              queries.startDate = { $lte: today };
+              queries.endDate = { $gte: today };
+              delete queries.filter;
+            } else if (queries.filter === "upcoming") {
+              query.startDate = { $gt: today };
+              queries.endDate = { $gt: today };
+              delete queries.filter;
+            } else if (queries.filter === "past") {
+              queries.endDate = { $lt: today };
+              delete queries.startDate;
+              delete queries.filter;
+            } else if (queries.filter === "week") {
+              let week = new Date();
+              week.setDate(week.getDate() - 7);
+              week = week.toISOString().split("T")[0] + "T00:00:00.000Z";
+              queries.startDate = { $gte: week };
+              queries.endDate = { $lt: today };
+              delete queries.filter;
+            } else if (queries.filter === "month") {
+              let month = new Date();
+              month.setMonth(month.getMonth() - 1);
+              month = month.toISOString().split("T")[0] + "T00:00:00.000Z";
+              queries.startDate = { $gte: month };
+              queries.endDate = { $lt: today };
+              delete queries.filter;
+            } else if (queries.filter === "year") {
+              let year = new Date();
+              year.setFullYear(year.getFullYear() - 1);
+              year = year.toISOString().split("T")[0] + "T00:00:00.000Z";
+              queries.startDate = { $gte: year };
+              queries.endDate = { $lt: today };
+              delete queries.filter;
+            }
+          }
+          const leaves = await Leave.find({
+            ...queries
+          })
+            .populate("userId", ["email", "_id"], "user")
+            .select("-__v");
+          if (leaves.length > 0) {
+            res.status(200).json({ success: true, leaves });
+          } else {
+            res.status(400).json({
+              success: false,
+              error: "No leaves found"
+            });
+          }
+        } else {
+          res.status(400).json({
+            success: false,
+            error: "Invalid Query Parameters"
+          });
+        }
+      } else {
+        res.status(400).json({ success: false, error: result.errors });
+      }
+    } catch (error) {
+      res.status(500).json({
+        success: false,
+        error: "Error Occurred on Server Side",
+        message: error.message
+      });
+    }
+  }
+);
+
 export default router;
