f432124ad1fd91121a283c28d5b38d853283d886
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-01-09 12:30:36 +0500

    feat: updated attendance routes to use verifyUserLogin middleware and improve error handling

--- routes/Attendance.js
+++ routes/Attendance.js
@@ -1,61 +1,90 @@
 import express from "express";
 import Attendance from "../models/Attendance.js";
-import verifyLogin from "../middlewares/verifyLogin.js";
+import verifyUserLogin from "../middlewares/verifyUserLogin.js";
 import Leave from "../models/Leave.js";
 
 const router = express.Router();
 
-router.route("/").post(verifyLogin, async (req, res) => {
-  try {
-    const userId = req.userId;
-    const date = new Date().toISOString().split("T")[0];
-    const attendance = await Attendance.findOne({
-      userId: userId,
-      date: date
-    });
-    const today = new Date().toISOString().split("T")[0];
-    let todayLeave = await Leave.find({
-      userId: userId,
-      $or: [{ status: "pending" }, { status: "approved" }],
-      startDate: { $lte: today },
-      endDate: { $gte: today }
-    }).distinct("userId");
-
-    if (attendance) {
-      res
-        .status(400)
-        .json({ success: false, error: "Attendance already marked" });
-    } else if (todayLeave.length > 0) {
-      res.status(400).json({
-        success: false,
-        error: "You are on leave, attendance cannot be marked"
+router
+  .route("/")
+  .post(verifyUserLogin, async (req, res) => {
+    try {
+      const userId = req.userId;
+      const date = new Date().toISOString().split("T")[0];
+      const attendance = await Attendance.findOne({
+        userId: userId,
+        date: date
       });
-    } else {
-      const todayStartTime = new Date().setHours(7, 0, 0, 0);
-      const currentTime = Date.now();
-      if (currentTime <= todayStartTime) {
-        await Attendance.create({
-          userId: userId,
-          status: "present"
+      const today = new Date().toISOString().split("T")[0];
+      let todayLeave = await Leave.find({
+        userId: userId,
+        $or: [{ status: "pending" }, { status: "approved" }],
+        startDate: { $lte: today },
+        endDate: { $gte: today }
+      }).distinct("userId");
+
+      if (attendance) {
+        res
+          .status(400)
+          .json({ success: false, error: "Attendance already marked" });
+      } else if (todayLeave.length > 0) {
+        res.status(400).json({
+          success: false,
+          error: "You are on leave, attendance cannot be marked"
         });
-        res.status({
+      } else {
+        const todayStartTime = new Date().setHours(7, 0, 0, 0);
+        const currentTime = Date.now();
+        if (currentTime <= todayStartTime) {
+          await Attendance.create({
+            userId: userId,
+            status: "present"
+          });
+          res.status({
+            success: true,
+            msg: `Your present is marked successfully`
+          });
+        } else {
+          res.status(400).json({
+            success: false,
+            error: "Attendance cannot be marked after 7 AM"
+          });
+        }
+      }
+    } catch (error) {
+      res.status(500).json({
+        success: false,
+        error: "Error Occurred on Server Side",
+        message: error.message
+      });
+    }
+  })
+  .get(verifyUserLogin, async (req, res) => {
+    try {
+      const userId = req.userId;
+      const date = new Date().toISOString().split("T")[0];
+      const attendance = await Attendance.findOne({
+        userId: userId,
+        date: date
+      });
+      if (attendance) {
+        res.status(200).json({
           success: true,
-          msg: `Your present is marked successfully`
+          attendance: attendance
         });
       } else {
         res.status(400).json({
           success: false,
-          error: "Attendance cannot be marked after 7 AM"
+          error: "Attendance is not marked yet"
         });
       }
+    } catch (error) {
+      res.status(500).json({
+        success: false,
+        error: "Error Occurred on Server Side",
+        message: error.message
+      });
     }
-  } catch (error) {
-    res.status(500).json({
-      success: false,
-      error: "Error Occurred on Server Side",
-      message: error.message
-    });
-  }
-});
+  });
 
 export default router;
