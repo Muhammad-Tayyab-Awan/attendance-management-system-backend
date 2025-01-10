d7da36ce3a2a05717c4aa804489456f98a36c0ff
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-01-10 09:52:58 +0500

    feat: added Grade model for tracking student attendance and performance metrics

---
+++ models/Grade.js
@@ -0,0 +1,40 @@
+import mongoose, { Schema } from "mongoose";
+
+const gradeSchema = new Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+      required: true
+    },
+    totalDays: {
+      type: Number,
+      required: true
+    },
+    totalPresents: {
+      type: Number,
+      required: true
+    },
+    totalAbsents: {
+      type: Number,
+      required: true
+    },
+    totalLeaves: {
+      type: Number,
+      required: true
+    },
+    percentage: {
+      type: Number,
+      required: true
+    },
+    grade: {
+      type: String,
+      required: true,
+      enum: ["A", "B", "C", "D", "E", "F"]
+    }
+  },
+  { timestamps: true }
+);
+
+const Grade = mongoose.model("grade", gradeSchema);
+export default Grade;
