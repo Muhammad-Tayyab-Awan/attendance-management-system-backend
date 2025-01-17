0fa0a469cab8a064db3205fac1f3140a9a9ac1bb
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-01-17 12:01:54 +0500

    fix: optimize absence tracking by filtering verified users and simplifying attendance logic

--- utils/absenceCron.js
+++ utils/absenceCron.js
@@ -1,7 +1,6 @@
 import cron from "node-cron";
 import User from "../models/Users.js";
 import Attendance from "../models/Attendance.js";
-import Leave from "../models/Leave.js";
 
 const absenceCronHour = process.env.ABSENCE_CRON_HOUR;
 const absenceCronMin = process.env.ABSENCE_CRON_MIN;
@@ -10,30 +9,29 @@ const startAbsenceCronJob = () => {
   try {
     const today = new Date();
     const todayISO = today.toISOString().split("T")[0];
-
     cron.schedule(
       `${absenceCronMin} ${absenceCronHour} * * *`,
       async () => {
         try {
-          const [attendedUsers, allUsers] = await Promise.all([
-            Attendance.find({ date: todayISO }),
-            User.find({ status: true, role: "user" }).select("-password")
-          ]);
-          const absentUsers = allUsers
-            .map((user) => user._id.toString())
-            .filter(
-              (userId) =>
-                !attendedUsers
-                  .map((user) => user.userId.toString())
-                  .includes(userId)
-            );
-          const newDate = new Date().toISOString().split("T")[0];
+          let attendedUsers = await Attendance.find({ date: todayISO });
+          let allUsers = await User.find({
+            role: "user",
+            status: true,
+            verified: true
+          });
+          attendedUsers = attendedUsers.map((user) => user.userId.toString());
+          allUsers = allUsers.map((user) => user._id.toString());
+          const absentUsers = allUsers.filter(
+            (user) => !attendedUsers.includes(user)
+          );
           if (absentUsers.length > 0) {
-            await Promise.all(
-              absentUsers.map((userId) =>
-                Attendance.create({ userId, status: "absent", date: newDate })
-              )
-            );
+            for (let absentUser of absentUsers) {
+              await Attendance.create({
+                userId: absentUser,
+                status: "absent",
+                date: todayISO
+              });
+            }
             console.log({
               success: true,
               msg: `Marked ${absentUsers.length} users as absent`
